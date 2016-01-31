26508c21c873c9808016bc42839bcd1b7fa909d6
prust <peter@cornerstonenw.com>
2016-01-30 22:35:48 -0800

    Added dragging of points, closes #1

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ canvas.style.height = height + 'px';
 window.show_points = true;
 window.control_point = false;
 
+// the distance between the mouse & any point
+// to make the mouse modify the point
+window.UI_SNAP = 5;
+
 var ctx = canvas.getContext("2d");
 var sprite, sprites = [];
 function createSprite() {
@@ -18,12 +22,58 @@ function createSprite() {
 }
 createSprite();
 
-document.body.addEventListener('click', function(evt) {
-  if (control_point)
-    sprite.addControlPoint({x: evt.clientX, y: evt.clientY});
-  else
-    sprite.addPoint({x: evt.clientX, y: evt.clientY});
+var dragging_pt = null;
+document.body.addEventListener('mousedown', function(evt) {
+  var pt = event2Point(evt);
+  var close_pt = _.find(sprite.points, function(sprite_pt) {
+    return distance(sprite_pt, pt) < UI_SNAP;
+  });
+  if (close_pt)
+    dragging_pt = close_pt;
 });
+
+document.body.addEventListener('mousemove', function(evt) {
+  var pt = event2Point(evt);
+  if (!dragging_pt)
+    return;
+
+  dragging_pt.x = pt.x;
+  dragging_pt.y = pt.y;
+});
+
+document.body.addEventListener('mouseup', function(evt) {
+  if (dragging_pt) {
+    dragging_pt = null;
+  }
+  else {
+    var pt = event2Point(evt);
+    if (control_point)
+      sprite.addControlPoint(pt);
+    else
+      sprite.addPoint(pt);
+  }
+});
+
+function distance(pt1, pt2) {
+  return Math.sqrt(square(Math.abs(pt1.x - pt2.x)) + square(Math.abs(pt1.y - pt2.y)));
+}
+
+function square(x) {
+  return Math.pow(x, 2);
+}
+
+// function intersects(max_pt1, min_pt1, max_pt2, min_pt2) {
+//   return !(
+//     max_pt1.x < min_pt2.x || 
+//     max_pt1.y < min_pt2.y || 
+//     min_pt1.x > max_pt2.x || 
+//     min_pt1.y > max_pt2.y
+//   );
+// }
+
+function event2Point(evt) {
+  return {x: evt.clientX, y: evt.clientY};
+}
 
 requestAnimationFrame(animate);
 animate();
